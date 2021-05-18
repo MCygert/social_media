863817cbc155611ec05f2a715ba20875f33781c7
Mikolaj Cygert <miki@comsite.com.pl>
2021-05-18 19:54:49 +0200

    Add index html page and styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import './App.css';
+import styled from 'styled-components';
 
 function App() {
   return (
     <div className="App">
-    </div>
+      <form>
+        <input type='text' value={'email'}></input>
+        <input type='password' value={'password'}></input>
+      </form>
+</div>
   );
 }
-
+const div = styled.div`
+  font-size: 20px
+  height: 55px
+`
 export default App;
