952ae76a8e16da4fa19b78c845c86546f10e2db7
Mikolaj Cygert <cygertmikolaj@gmail.com>
2021-05-24 19:42:10 +0200

    Add simple data to backend part of application

--- server/index.ts
+++ server/index.ts
@@ -2,16 +2,31 @@ import express from 'express';
 const app = express();
 const port = 8080;
 interface User {
-	name: string;
-	age: number;
+	id: number;
+	firstName: String;
+	lastName: String;
+	email: String;
+	gender: String;
+	password: String;
+	friends: Array<Name>;
 }
+interface Name {
+	firstName: String;
+	lastName: String;
+}
+interface loginUser {
+	email: String;
+	password: String;
+}
+const users: Array<User> = require('./MOCK_DATA.json');
+app.use(express.json());
 
 app.get('/', (req, res) => {
-	const mikolaj: User = {
-		age: 23,
-		name: 'Mikolaj',
-	};
-	res.send(mikolaj);
+	res.send(users);
+});
+app.post('/', (req, res) => {
+	const requestData: loginUser = req.body;
+	res.send(requestData);
 });
 app.listen(port, () => {
 	console.log(`Listening on port http://localhost:${port}`);
