027a7e1bb3a4fe87273cf641ce7fc3cd9be457cc
Mikolaj Cygert <cygertmikolaj@gmail.com>
2021-06-01 22:12:16 +0200

    Adding fetching in react part

--- src/form/Form.tsx
+++ src/form/Form.tsx
@@ -1,6 +1,11 @@
+import { SyntheticEvent } from 'react';
 import styled from 'styled-components';
+const axios = require('axios').default;
 
 export const Form = () => {
+	const headers = {
+		'Access-Control-Allow-Origin': 'http://localhost:8080'
+	}
 	const Form = styled.form`
 		font-size: 21px;
 		height: 55px;
@@ -18,13 +23,31 @@ export const Form = () => {
 		display: grid;
 		grid-template-columns: auto auto;
 	`;
+	const getUser = async (e: SyntheticEvent) => {
+		e.preventDefault();
+		try{
+		const response = await axios.post('http://localhost:8080/', {
+			email: 'test',
+			pasword: 'test',
+		},{
+			headers: headers
+		});
+		console.log(response)
+	}
+	catch (err) {
+		console.log(err)
+	}
+	};
 	return (
 		<Form>
 			<input type='text' value={'email'}></input>
 			<input type='password' value={'password'}></input>
 			<ButtonsSection>
 				<LoginButton type='button' value={'register'}></LoginButton>
-				<LoginButton type='button' value={'login'}></LoginButton>
+				<LoginButton
+					type='button'
+					value={'login'}
+					onClick={getUser}></LoginButton>
 			</ButtonsSection>
 		</Form>
 	);
